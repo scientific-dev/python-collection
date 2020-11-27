8ce675d81d84d6b58cafd8deaaa0ac89d1010a70
Science Spot <69678617+Scientific-Guy@users.noreply.github.com>
2020-11-27 14:16:24 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 if [ $1 == 'test' ] 
 then 
-    C:/windows/py.exe test.py 
+    python test.py 
 elif [ $1 == 'build' ]
 then
-    C:/windows/py.exe setup.py sdist bdist_wheel
+    python setup.py sdist bdist_wheel
 
-    if [ $1 == '-u' ]
+    if [ $2 == '-u' ]
     then
-        C:/windows/py.exe -m twine upload dist/*
+        python -m twine upload dist/*
     fi
 elif [ $1 == 'upload' ]
 then
-    C:/windows/py.exe -m twine upload dist/*
+    python -m twine upload dist/*
 elif [ $1 == 'commit' ]
 then
     echo "Commit Description: "
